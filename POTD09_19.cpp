dd2bb7688e5ec72eedee33243191ba64aa6701f0
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2024-09-19 23:12:44 +0530

    Reverse Words

---
+++ POTD09_19.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to reverse words in a given string.
+    string reverseWords(string str) {
+        // code here
+        vector<string> words;
+    string word = "";
+    
+    for (char c : str) {
+        if (c == '.') {
+            words.push_back(word);
+            word = "";
+        } else {
+            word += c;
+        }
+    }
+    
+    words.push_back(word);
+    string result = "";
+    
+    for (int i = words.size() - 1; i >= 0; --i) {
+        result += words[i];
+        if (i > 0) {
+            result += '.';
+        }
+    }
+    
+    return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout << obj.reverseWords(s) << endl;
+    }
+}
+// } Driver Code Ends
