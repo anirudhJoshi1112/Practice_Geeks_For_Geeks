d3c2b991071aed619991aa6e3677a4b21987fda8
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2024-10-09 18:27:46 +0530

    Closest String

---
+++ GFG_SDE_SHEET05.cpp
@@ -0,0 +1,24 @@
+class Solution{
+public:	
+	
+	int shortestDistance(vector<string> &s, string word1, string word2)
+	{
+		// Your code goes here
+		int d1=-1;
+		int d2=-1;
+		int ans=INT_MAX;
+		
+		for(int i=0;i<s.size();i++){
+		    if(s[i]==word1){
+		        d1=i;
+		    }
+		    if(s[i]==word2){
+		        d2=i;
+		    }
+		    if(d1!=-1&&d2!=-1){
+		        ans=min(ans,abs(d1-d2));
+		    }
+		}
+		 return ans;
+	}
+};
