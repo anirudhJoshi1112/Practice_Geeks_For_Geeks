e28f4d223c0e7033246d1348ca2773b7730f7289
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2024-11-23 09:16:57 +0530

    Move All Zeros to End

--- POTD11_16.cpp
+++ POTD11_16.cpp
@@ -1,3 +1,4 @@
+//method - 1
 class Solution {
   public:
     void pushZerosToEnd(vector<int>& arr) {
@@ -9,6 +10,29 @@ class Solution {
 	            j++;
 	        }
 	    }
+        
+    }
+};
+//method - 2
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        vector<int> temp(n);
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                temp[j++]=arr[i];
+            }
+        }
+        
+        while(j<n){
+            temp[j++]=0;
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=temp[i];
+        }
         
     }
 };
